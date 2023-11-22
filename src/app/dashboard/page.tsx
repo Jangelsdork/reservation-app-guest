5735049c27ead98a86d12c89b9fa34f1d2c7624f
Jack Mangelsdorf <j.hespe.mangelsdorf@gmail.com>
2023-11-22 16:16:19 +0100

    added calendar in main dashboard view. To do: Styling

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,36 @@
+"use client"
 import React from 'react'
 import Sidebar from '@/components/Sidebar'
+import { useState } from 'react';
+import { RxHamburgerMenu } from "react-icons/rx";
+import { Calendar } from 'antd';
+
+
 
 type Props = {}
 
 const index = (props: Props) => {
+
+  const [isOpen, setIsOpen] = useState(true);
+
   return (
-    <div>
-      <Sidebar />
+    <div className="flex h-screen bg-gray-200">
+      <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
+      {/* Main content */}
+      <div className='flex flex-row w-[100vw] justify-center align-end bg-neutral-800'>
+        <div className="flex-1 p-8">
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            <RxHamburgerMenu />
+
+          </button>
+          <div className='w-[80vw]'>
+            <Calendar className=''/>
+          </div>
+          {/* Main content goes here */}
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,7 @@
 "use client"
 import React, { useState } from 'react';
 import { Transition } from '@headlessui/react';
-import { RxHamburgerMenu } from "react-icons/rx";
 import { TfiArrowCircleLeft } from "react-icons/tfi";
-import { FaCalendar } from "react-icons/fa";
 import { BsCalendar, BsPeople, BsShop } from "react-icons/bs";
 
 
@@ -12,11 +10,10 @@ import Avvvatars from 'avvvatars-react'
 
 
 
-const CollapsibleSidebar: React.FC = () => {
-  const [isOpen, setIsOpen] = useState(true);
+const CollapsibleSidebar: React.FC = ({ isOpen, setIsOpen }) => {
 
   return (
-    <div className="flex h-screen bg-gray-200">
+    <div >
       {/* Sidebar */}
       <Transition
         show={isOpen}
@@ -30,42 +27,28 @@ const CollapsibleSidebar: React.FC = () => {
         <div className="fixed inset-y-0 left-0 z-50 w-64 bg-neutral-900 shadow-xl pl-2 pr-2">
           <div id='container' className="flex flex-col w-100%">
             <div id='collapse_icon' className='flex flex-row justify-end pr-2 sm:pr-4 pt-10'>
-            <button
-          onClick={() => setIsOpen(!isOpen)}
-        ><TfiArrowCircleLeft />
+              <button
+                onClick={() => setIsOpen(!isOpen)}
+              ><TfiArrowCircleLeft />
 
-        </button>
+              </button>
             </div>
             <div id='user_block' className='p-4 flex flex-row items-center border-b border-neutral-500'>
               <div id='avatar'> <Avvvatars value="JM" /> </div> {/*to do: get avatar url, load avatar (or, change avvvatars value) */}
               <div className='flex flex-col pl-4'>
-              <h1 className='text-whitete xt-xl font-bold '>Jack Mangelsdorf</h1> {/*to do: get user name*/}
-              <h2 className='text-xs '>Super User</h2> {/*to do: get role*/}
+                <h1 className='text-whitete xt-xl font-bold '>Jack Mangelsdorf</h1> {/*to do: get user name*/}
+                <h2 className='text-xs '>Super User</h2> {/*to do: get role*/}
               </div>
             </div>
             <div id='menu' className=' flex flex-col h-[50vh] gap-8 pt-10'>
               <div className='flex flex-row  items-center gap-4 pl-8'><BsCalendar /> Bookings</div>
-              <div className='flex flex-row  items-center gap-4 pl-8'><BsShop />
- Manage venue</div>
+              <div className='flex flex-row  items-center gap-4 pl-8'><BsShop />Manage venue</div>
               <div className='flex flex-row  items-center gap-4 pl-8'><BsPeople /> Manage People</div>
-          
+
             </div>
           </div>
         </div>
       </Transition>
-
-      {/* Main content */}
-      <div className='flex flex-row w-[100vw] justify-center align-end bg-neutral-800'>
-      <div className="flex-1 p-8">
-        <button
-          onClick={() => setIsOpen(!isOpen)}
-        >
-         <RxHamburgerMenu />
-
-        </button>
-        {/* Main content goes here */}
-      </div>
-      </div>
     </div>
   );
 };
