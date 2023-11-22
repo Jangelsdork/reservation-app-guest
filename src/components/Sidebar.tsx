e3221875ba599ac293297059e1f36aebb0d2205b
Jack Mangelsdorf <j.hespe.mangelsdorf@gmail.com>
2023-11-22 12:59:31 +0100

    create user skeleton in sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,6 +3,8 @@ import React, { useState } from 'react';
 import { Transition } from '@headlessui/react';
 import { RxHamburgerMenu } from "react-icons/rx";
 import { TfiArrowCircleLeft } from "react-icons/tfi";
+import Avvvatars from 'avvvatars-react'
+
 
 
 
@@ -23,12 +25,19 @@ const CollapsibleSidebar: React.FC = () => {
       >
         <div className="fixed inset-y-0 left-0 z-50 w-64 bg-neutral-900 shadow-xl">
           <div id='container' className="flex flex-col w-100%">
-            <div id='collapse' className='flex flex-row justify-end pr-2 sm:pr-4 pt-10'>
+            <div id='collapse_icon' className='flex flex-row justify-end pr-2 sm:pr-4 pt-10'>
             <button
           onClick={() => setIsOpen(!isOpen)}
         ><TfiArrowCircleLeft />
 
         </button>
+            </div>
+            <div id='user_block' className='p-4 flex flex-row items-center border-b border-neutral-500 ml-2'>
+              <div id='avatar'> <Avvvatars value="JM" /> </div> {/*to do: get avatar url, load avatar (or, change avvvatars value) */}
+              <div className='flex flex-col pl-4'>
+              <h1 className='text-whitete xt-xl font-bold '>Jack Mangelsdorf</h1> {/*to do: get user name*/}
+              <h2 className='text-xs '>Super User</h2> {/*to do: get role*/}
+              </div>
             </div>
           </div>
         </div>
