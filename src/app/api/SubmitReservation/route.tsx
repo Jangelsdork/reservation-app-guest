b65268cbc31a81f7b0279774525716f169326289
jangelsdork <j.hespe.mangelsdorf@gmail.com>
2023-12-09 17:40:52 +0100

    editing api route. to do: add async function to form, that passes body to api

--- src/app/api/SubmitReservation/route.tsx
+++ src/app/api/SubmitReservation/route.tsx
@@ -1,11 +1,28 @@
 import { PrismaClient } from "@prisma/client"
 import { NextResponse } from "next/server";
 
+type Booking = {
+  first_name: string;
+  last_name: string;
+  email: string;
+  phone: string;
+  marketing_consent: boolean; 
+  booking_date: string;
+  start_time: string; 
+  end_time: String;
+  party_size: number; 
+}
+
 
 
 const prisma = new PrismaClient()
 
-export  async function GET(request) {
-  const posts = await prisma.promoter.findMany()
-  return NextResponse.json(posts)
+// eslint-disable-next-line import/prefer-default-export
+export  async function POST(req: Request) {
+  const data = await req.json();
+  console.log(data)
+
+  return new Response("success")
+  // const posts = await prisma.promoter.findMany()
+  // return NextResponse.json(posts)
 }
