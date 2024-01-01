afd8fce7ed86f4616c8f3ee6c394c9210d238f50
jangelsdork <j.hespe.mangelsdorf@gmail.com>
2024-01-01 17:48:54 +0100

    removed sidebar component, replaced with shadcn sheet component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,16 +2,20 @@
 
 import React from "react";
 import Sidebar from "@/components/Sidebar";
-// import { useState } from "react";
-import { RxHamburgerMenu, RxInfoCircled } from "react-icons/rx";
+import { useState } from "react";
 import { columns } from "./columns";
 import { DataTable } from "./dataTable";
+import SheetComp from "@/components/SheetComp"
 
 async function getData(): Promise<[]> {
-  const response = await fetch("http://localhost:3000/api/getUpcomingBookings", {
-    cache: "no-store",
-  });
+  const response = await fetch(
+    "http://localhost:3000/api/getUpcomingBookings",
+    {
+      cache: "no-store",
+    }
+  );
   const returnedData = await response.json();
+
   console.log(returnedData);
   return returnedData.bookings.map((x) => ({
     date: x.booking_date,
@@ -22,19 +26,15 @@ async function getData(): Promise<[]> {
   }));
 }
 
+// eslint-disable-next-line @next/next/no-async-client-component
 export default async function Index() {
-  // const [isOpen, setIsOpen] = useState<boolean>(true);
   const data = await getData();
   return (
     <div className="absolute delay-300 w-100% flex min-h-screen bg-gray-200">
-      {/* <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} /> */}
       {/* Main content */}
       <div className="flex flex-row w-[100vw] justify-center align-end bg-neutral-800">
         <div className="flex-1 p-8">
-          {/* <button onClick={() => setIsOpen(!isOpen)}> */}
-          <RxHamburgerMenu />
-          {/* </button> */}
-          <div className="w-[80vw]">{/* <Calendar className=''/> */}</div>
+          <SheetComp /> 
           {/* Main content goes here */}
           <div className="pl-64">
             <div className="container mx-auto py-10">
