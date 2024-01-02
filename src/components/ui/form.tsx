e51bc7df3af6490e4a695c4a27dd38a6fe023c14
jangelsdork <j.hespe.mangelsdorf@gmail.com>
2024-01-02 15:17:54 +0100

    bug fix

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -60,6 +60,9 @@ const useFormField = () => {
   }
 }
 
+type FormItemContextValue = {
+  id: string
+}
 
 const FormItemContext = React.createContext<FormItemContextValue>(
   {} as FormItemContextValue
